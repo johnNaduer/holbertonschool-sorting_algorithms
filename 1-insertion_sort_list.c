2877b1d6b839d42ad32bf69a5843ace3b9861e66
johnNaduer <naduer789@gmail.com>
2022-12-26 18:27:23 -0800

    add new changes

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,31 +1,46 @@
 #include "sort.h"
-void insertion_sort_list(listint_t **list) {
-  listint_t *current, *temp;
 
-  if (list == NULL || *list == NULL || (*list)->next == NULL) {
-    return;
-  }
+/**
+ * insertion_sort_list - function that sorts a doubly linked list of integers
+ * in ascending order using the Insertion sort algorithm
+ * @list: input listint_t
+ *
+ * Return: Always 0
+ */
 
-  current = (*list)->next;
+void insertion_sort_list(listint_t **list)
+{
+	listint_t *current, *temp;
 
-  while (current != NULL) {
-    temp = current;
-    while (temp->prev != NULL && temp->prev->n > temp->n) {
-      temp->prev->next = temp->next;
-      if (temp->next != NULL) {
-        temp->next->prev = temp->prev;
-      }
-      temp->next = temp->prev;
-      temp->prev = temp->prev->prev;
-      temp->next->prev = temp;
-      if (temp->prev != NULL) {
-        temp->prev->next = temp;
-      }
-      else {
-        *list = temp;
-      }
-      print_list(*list);
-    }
-    current = current->next;
-  }
+	if (list == NULL || *list == NULL || (*list)->next == NULL)
+	{
+	return;
+	}
+
+	current = (*list)->next;
+	while (current != NULL)
+	{
+	temp = current;
+	while (temp->prev != NULL && temp->prev->n > temp->n)
+	{
+	temp->prev->next = temp->next;
+	if (temp->next != NULL)
+	{
+	temp->next->prev = temp->prev;
+	}
+	temp->next = temp->prev;
+	temp->prev = temp->prev->prev;
+	temp->next->prev = temp;
+	if (temp->prev != NULL)
+	{
+	temp->prev->next = temp;
+	}
+	else
+	{
+	*list = temp;
+	}
+	print_list(*list);
+	}
+	current = current->next;
+	}
 }
